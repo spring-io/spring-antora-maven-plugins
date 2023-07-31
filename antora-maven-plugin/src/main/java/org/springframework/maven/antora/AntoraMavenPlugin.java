c166759c52aec620cfaf951e9cee18381afd58bb
Rob Winch <rwinch@users.noreply.github.com>
2023-07-31 14:45:29 -0500

    Add antora lifecycle
    Closes gh-4

--- antora-maven-plugin/src/main/java/org/springframework/maven/antora/AntoraMavenPlugin.java
+++ antora-maven-plugin/src/main/java/org/springframework/maven/antora/AntoraMavenPlugin.java
@@ -21,10 +21,7 @@ import org.apache.maven.model.Plugin;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.BuildPluginManager;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugins.annotations.Component;
-import org.apache.maven.plugins.annotations.LifecyclePhase;
-import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.*;
 import org.apache.maven.project.MavenProject;
 import org.twdata.maven.mojoexecutor.MojoExecutor;
 
@@ -38,7 +35,7 @@ import static org.twdata.maven.mojoexecutor.MojoExecutor.*;
 /**
  * Runs Antora within a Maven project.
  */
-@Mojo(name = "antora", defaultPhase = LifecyclePhase.SITE)
+@Mojo(name = "antora", defaultPhase = LifecyclePhase.NONE)
 public class AntoraMavenPlugin
 		extends AbstractMojo {
 
